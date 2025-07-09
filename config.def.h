8223504d48f260c2c8bf709d6e53883b42962095
zhmlst <zhmlst.btw@gmail.com>
2025-07-09 15:51:19 +0300

    preparing config.def.h for the release

--- config.def.h
+++ config.def.h
@@ -1,25 +1,15 @@
-static const char spinner[] = R"(#!/bin/sh
-spinner='\|/-'
-while :; do
-  for ((i=0; i<${#spinner}; i++)); do
-    printf " %s waiting..." "${spinner:$i:1}"
-    sleep 0.3
-  done
-done
-)";
-
+// config.h
 static const char counter[] = R"(#!/bin/sh
-counter=0
-echo $counter
-while read _; do
-  ((counter++))
-  echo $counter
-done
+	counter=0
+	echo $counter
+	while read _; do
+	  ((counter++))
+	  echo $counter
+	done
 )";
 
 static const Spec specs[] = {
 	/* command, signal */
-	{ spinner },
 	{ counter, 1 },
 	{ "while :; do date +'%m.%d %H:%M' && sleep 60; done" },
 };
